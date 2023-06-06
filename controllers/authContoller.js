dde2d44f6b3e7569d8a37f9ad2b6e0d1d90b4adb
Nicolas Arciniegas <nicolasarci96@gmail.com>
2023-06-06 14:17:31 -0500

    se conecto el jwt para obtener los tokens de autentificacion a la hora de realizar el login

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
 const userModel = require("../models/userModel");
 
 //buscar y validar el usuario exista
@@ -17,7 +18,8 @@ userModel
                 res.status(500).json({error:err.message})
             }else if(result){
                 //si la contrasena coincide el usuario se autentifica exitosamente
-                res.status(200).json({message:"authentification succesful"})
+                const token = jwt.sign({userId:user.id},"secreto",{expiresIn: '1h'});
+                res.status(200).json({message:"authentification succesful", token})
             }else{
                 // si la contrasena no coincide, se devuelve un mensaje de error
                 res.status(401).json({message:"authentification failed"})
